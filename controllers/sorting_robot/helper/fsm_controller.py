7d423697f4519fbee854709f571ff2c5c0e97c9c
Kaden Almizyed <kalmizyed@wisc.edu>
2025-11-30 17:00:15 -0600

    Borrow from HW4 and get basic pickup/drop functionality working w/ perception

---
+++ controllers/sorting_robot/helper/fsm_controller.py
@@ -0,0 +1,222 @@
+# you may find all of these packages helpful!
+from scipy.spatial.transform import Rotation as R
+import math
+import numpy as np
+import cv2
+import helper.inverse_kinematics as IK
+import helper.trajectory_generator as Traj
+
+# This is the starter class you can modify with whatever methods/members you might need!
+class RobotPlacerWithVision():
+
+    # COORDINATES RELATIVE TO CAMERA TABLE VIEW:
+    # X = UP/DOWN, higher = camera moves down, range ~ [0.4, 0.7]
+    # Y = LEFT/RIGHT, higher = camera moves right, range ~ [-0.05, 0.35]
+    # Z = DEPTH
+
+    # Mat center in world: -0.1 2.75 0.7, so mat->robot = [0.1, -0.55, 0] turned 90 deg left = [-0.55, -0.1, 0], mat->camera = [-0.55, -0.15, 0]
+    # Camera: Robot->Camera = [-0.05 0 0], Camera->Robot = [0.05 0 0], Camera->Mat = [0.55, 0.15, 0]
+    # Robot center in world: 0 2.2 0.7, rotated 90 deg around Z (left)
+    # Camera rotation: -90 deg around Z (right), so camera->robot = 90 deg left
+
+    CAMERA = {
+        'fovx': 1.0472,
+        'near': 0.01,
+        'far': 3,
+        'width': 640,
+        'height': 480
+    }
+
+    COLORS = {
+        'red': [1, 0, 0],
+        'blue': [0, 0, 1]
+    }
+    __blocks__ = ['red']
+
+    BLOCK_HEIGHT = 0.03
+    CONVEYOR_Z_OFFSET = 0.1
+
+    initial_position = [0.65, 0.15, 0.65]
+    initial_rotation = [0, 0, -math.pi/2]
+    drop_prep_position = [0.45, -0.15, 0.6]
+    drop_position = [0, -0.6, 0.6]
+    drop_post_position = [0.45, -0.15, 0.65]
+    
+    gripperClosed = False
+    gripperCloseTime = 32 # Timesteps
+    gripperOpenTime = 16 # Timesteps
+    cameraWaitTime = 32 # Timesteps
+
+    def __init__(self):
+        self.fsmState = self.resetToDefault
+        self.__block_cur_index__ = 0
+        self.trajectory = {}
+
+        # Camera intrinsics
+        self.CAMERA['fovy'] = 2 * math.atan(math.tan(self.CAMERA['fovx']*0.5) * (self.CAMERA['height']/self.CAMERA['width']))
+        self.CAMERA['fx'] = self.CAMERA['width']/(2*math.tan(self.CAMERA['fovx']/2))
+        self.CAMERA['fy'] = self.CAMERA['height']/(2*math.tan(self.CAMERA['fovy']/2))
+
+    def getRobotCommand(self, tt, current_q, current_image_bgr):
+
+        # This is the code we will call. Please don't change the signature!
+        
+        # Update current joints and task-space transformations
+        self.current_joints = current_q
+        self.next_joints = current_q
+        current_transform = IK.getFK(current_q)
+        self.current_position = current_transform[:3,3]
+        self.current_rotation = R.from_matrix(current_transform[:3,:3]).as_rotvec()
+
+        # Update current time
+        self.timestep = tt
+
+        # Update current image
+        self.current_image = current_image_bgr
+
+        # Call current FSM function
+        self.fsmState()
+        
+        # you should return a 7-length list with the desired joint angles and the gripper value
+        # e.g., the following line would be for a random configuration with the gripper closed
+        return self.next_joints + [self.gripperClosed]
+    
+
+    ### Helper functions
+
+    def getPixelLocations(self):
+        color = self.COLORS[self.block_cur]
+        b, g, r = cv2.split(self.current_image)
+
+        r = cv2.threshold(r, 200*color[0], 255*color[0], cv2.THRESH_BINARY)[1]
+        g = cv2.threshold(g, 200*color[1], 255*color[1], cv2.THRESH_BINARY)[1]
+        b = cv2.threshold(b, 200*color[2], 255*color[2], cv2.THRESH_BINARY)[1]
+        merged = cv2.merge([b, g, r])
+        objects = cv2.threshold(cv2.cvtColor(merged, cv2.COLOR_BGR2GRAY), 1, 255, cv2.THRESH_BINARY)[1]
+        
+        contours, hierarchy = cv2.findContours(objects, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        centers = []
+        for i in contours:
+            M = cv2.moments(i)
+            if M['m00'] != 0:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                centers.append((cx, cy))
+        
+        return centers
+    
+    
+    ### FSM States
+
+    # Reset to default position
+    def resetToDefault(self):
+        self.desired_pose = self.initial_position + self.initial_rotation
+        self.gripperClosed = False
+        self.fsmState = self.generateTrajectory
+        print("RESET") # TODO remove
+
+    def prepMoveToBin(self):
+        self.desired_pose = self.drop_prep_position + self.initial_rotation
+        self.fsmState = self.generateTrajectory
+        print("PREP") # TODO remove
+    
+    def postMoveToBin(self):
+        self.desired_pose = self.drop_post_position + self.initial_rotation
+        self.fsmState = self.generateTrajectory
+        print("POST") # TODO remove
+
+    # Set goal position to bin
+    def moveToBin(self):
+        self.desired_pose = self.drop_position + self.initial_rotation
+        self.fsmState = self.generateTrajectory
+        print("MOVE TO BIN") # TODO remove
+
+    # Decide next block to get, block_cur
+    def decideNextBlock(self):
+        # If no blocks left, move to end state
+        if self.__block_cur_index__ == len(self.__blocks__):
+            self.fsmState = self.endState
+        
+        # Else, get next block and move to next state
+        else:
+            self.block_cur = self.__blocks__[self.__block_cur_index__]
+            self.pauseStartTime = self.timestep
+            self.fsmState = self.getBlockLocation
+
+    # Get location of current desired block
+    def getBlockLocation(self):
+        # Wait for a small number of timesteps to ensure camera is done moving
+        if self.timestep - self.pauseStartTime < self.cameraWaitTime:
+            return
+
+        centers = self.getPixelLocations()
+
+        # Assumes only one object center is returned
+        u, v = centers[0]
+        
+        # convert pixel location to camera-space location
+        cx = self.CAMERA['width']/2
+        cy = self.CAMERA['height']/2
+        z = self.current_position[2] - self.CONVEYOR_Z_OFFSET
+        x = (u - cx)*(z/self.CAMERA['fx'])
+        y = -(v - cy)*(z/self.CAMERA['fy'])
+
+        # Move from current camera position to object location,
+        # accounting for block height, camera offset, and gripper height
+        block_position = np.array(self.current_position) + np.array([-y, x-0.05, 0])
+        block_position[2] = 0.165
+        block_position[2] += self.CONVEYOR_Z_OFFSET
+        self.desired_pose = block_position.tolist() + self.initial_rotation
+        self.fsmState = self.generateTrajectory
+
+
+    # Generate trajectory to current desired position
+    def generateTrajectory(self):
+        desired_joints = IK.getInverseKinematics(self.desired_pose, self.current_joints)
+        self.trajectory['T'] = Traj.getRequiredTime(self.current_joints, desired_joints)
+        self.trajectory['t'] = 0
+        self.trajectory['start'] = self.current_joints
+        self.trajectory['end'] = desired_joints
+        self.fsmState = self.moveToLocation
+
+    # Given desired location and current location, update the next joint
+    # position along the trajectory until the goal is reached
+    def moveToLocation(self):
+        # Move to next position along trajectory
+        self.next_joints = Traj.getPosition(self.trajectory['start'], self.trajectory['end'], self.trajectory['t'], self.trajectory['T'])
+        self.trajectory['t'] = min(self.trajectory['t'] + 1, self.trajectory['T'])
+        
+        if self.trajectory['t'] == self.trajectory['T']:
+            self.trajectory = {}
+            if self.desired_pose == self.initial_position + self.initial_rotation:
+                self.fsmState = self.decideNextBlock
+            elif self.desired_pose == self.drop_prep_position + self.initial_rotation:
+                self.fsmState = self.moveToBin
+            elif self.desired_pose == self.drop_position + self.initial_rotation:
+                self.fsmState = self.dropAndMarkDone
+            elif self.desired_pose == self.drop_post_position + self.initial_rotation:
+                self.fsmState = self.resetToDefault
+            else:
+                self.fsmState = self.gripBlock
+
+    # Grip block_cur, wait for enough time for the gripper to fully close, then move on
+    def gripBlock(self):
+        if not self.gripperClosed:
+            self.gripperClosed = True
+            self.pauseStartTime = self.timestep
+        elif self.timestep - self.pauseStartTime >= self.gripperCloseTime:
+            self.fsmState = self.prepMoveToBin
+
+    # Drop block_cur; once gripper is fully open, verify if block is in basket,
+    # then update block_cur_index and move to first state
+    def dropAndMarkDone(self):
+        if self.gripperClosed:
+            self.gripperClosed = False
+            self.pauseStartTime = self.timestep
+        elif self.timestep - self.pauseStartTime >= self.gripperOpenTime:
+            self.__block_cur_index__ += 1
+            self.fsmState = self.postMoveToBin
+            
+
+    def endState(self):
+        pass
--- controllers/sorting_robot/sorting_robot.py
+++ controllers/sorting_robot/sorting_robot.py
@@ -1,57 +1,151 @@
 """sorting_robot controller."""
 
-from controller import Robot
-from controller import Supervisor
-from helper.pose_finder import getOptimalPose
-from helper.kinematics_controller import kinematics_controller
+from controller import Robot, Supervisor
+import numpy as np
+from helper.fsm_controller import RobotPlacerWithVision
 
-### Tuning
-IK_WINDOW = 20
-    
+# ===== Optional: fast live preview with OpenCV =====
+try:
+    import cv2
+    _HAS_CV2 = True
+except Exception:
+    _HAS_CV2 = False
+    print("[camera] OpenCV not available; will skip live preview.")
 
-### Main Script
-
-# create the Robot instance.
+# -----------------------------------------------------------------------------
+# Init
+# -----------------------------------------------------------------------------
 robot = Supervisor()
-
-# get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# Get all motors (joint actuators)
-motors = []
-motors.append(robot.getDevice("shoulder_pan_joint"))
-motors.append(robot.getDevice("shoulder_lift_joint"))
-motors.append(robot.getDevice("elbow_joint"))
-motors.append(robot.getDevice("wrist_1_joint"))
-motors.append(robot.getDevice("wrist_2_joint"))
-motors.append(robot.getDevice("wrist_3_joint"))
-
-# TODO add gripper joint
-
-# Enable joint position sensors (radians)
-sensors = []
+# -----------------------------------------------------------------------------
+# UR5e arm joints (6-DoF)
+# -----------------------------------------------------------------------------
+arm_joint_names = [
+    "shoulder_pan_joint",
+    "shoulder_lift_joint",
+    "elbow_joint",
+    "wrist_1_joint",
+    "wrist_2_joint",
+    "wrist_3_joint",
+]
+motors = [robot.getDevice(n) for n in arm_joint_names]
 for m in motors:
     ps = m.getPositionSensor()
     ps.enable(timestep)
-    sensors.append(ps)
 
-# Main loop:
+# -----------------------------------------------------------------------------
+# Robotiq 2F-85 gripper (two finger motors under toolSlot)
+# Names come from your world: Robotiq2f85Gripper { name "robotiq_2f85" }
+# So the device names are:
+#   "robotiq_2f85::left finger joint", "robotiq_2f85::right finger joint"
+# -----------------------------------------------------------------------------
+GRIP_MIN = 0.0
+GRIP_MAX = 0.80  # typical range on actuated finger joints; adjust if needed
+
+def _safe_get_device(name):
+    try:
+        return robot.getDevice(name)
+    except Exception:
+        return None
+
+gripper_left  = _safe_get_device("robotiq_2f85::left finger joint")
+gripper_right = _safe_get_device("robotiq_2f85::right finger joint")
+
+if gripper_left and gripper_right:
+    for g in (gripper_left, gripper_right):
+        try:
+            g.setVelocity(1.5)
+        except Exception:
+            pass
+        try:
+            g.setForce(50.0)
+        except Exception:
+            pass
+    print("[gripper] Robotiq 2F-85 finger motors found.")
+else:
+    print("[gripper] Could not find Robotiq 2F-85 motors. "
+          "Expected: 'robotiq_2f85::left finger joint' and "
+          "'robotiq_2f85::right finger joint'.")
+
+def set_gripper_q(q):
+    """Set finger joint target (radians) to both fingers."""
+    if not (gripper_left and gripper_right):
+        return
+    q = float(np.clip(q, GRIP_MIN, GRIP_MAX))
+    gripper_left.setPosition(q)
+    gripper_right.setPosition(q)
+
+def set_gripper_normalized(g):
+    """g in [0,1]: 0=open, 1=closed."""
+    g = float(np.clip(g, 0.0, 1.0))
+    q = GRIP_MIN + g * (GRIP_MAX - GRIP_MIN)
+    set_gripper_q(q)
+
+def set_gripper_width_mm(width_mm, max_open_mm=85.0):
+    """Approximate map from opening width (mm) to joint value; tune as needed."""
+    width_mm = float(np.clip(width_mm, 0.0, max_open_mm))
+    set_gripper_normalized(1.0 - width_mm / max_open_mm)
+
+# -----------------------------------------------------------------------------
+# Wrist camera (correct BGRA → BGR for OpenCV preview)
+# -----------------------------------------------------------------------------
+cam = _safe_get_device("wrist_camera")
+if cam:
+    cam.enable(timestep)
+    cam_w, cam_h = cam.getWidth(), cam.getHeight()
+    print(f"[camera] Enabled wrist_camera at {cam_w}x{cam_h}")
+else:
+    cam_w = cam_h = None
+    print("[camera] No device named 'wrist_camera' found.")
+
+def _camera_step_preview():
+    if not (cam and _HAS_CV2):
+        return
+    img_bytes = cam.getImage()
+    if img_bytes is None:
+        return
+    # Webots returns BGRA
+    bgra = np.frombuffer(img_bytes, dtype=np.uint8).reshape((cam_h, cam_w, 4))
+    bgr = cv2.cvtColor(bgra, cv2.COLOR_BGRA2BGR)
+    cv2.imshow("Wrist Camera", bgr)
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC closes
+        cv2.destroyAllWindows()
+    return bgr
+
+# -----------------------------------------------------------------------------
+# Main loop
+# -----------------------------------------------------------------------------
 tt = 0
 
-# - perform simulation steps until Webots is stopping the controller
-last_q = []
-for sensor in sensors:
-    last_q.append(sensor.getValue())
+rpwv = RobotPlacerWithVision()
 
-kinematics = kinematics_controller(IK_WINDOW)
-    
-while robot.step(timestep) != -1:
-    desired_pose = getOptimalPose()
-    desired_command = kinematics.getCommand(tt, desired_pose, last_q)
-    last_q = desired_command
-    
-    # 7 Robot Joints
-    for j, motor in enumerate(motors):
-        motor.setPosition(desired_command[j])
+current_q  = [-0.343, -1.2, 1.5, -2.0, -1.57, 1.03]
+
+try:
+    while robot.step(timestep) != -1:
         
-    tt+=1
+        bgr = _camera_step_preview()
+
+        desired_command = rpwv.getRobotCommand(tt, current_q, bgr)
+
+        current_q = desired_command[:-1]
+
+        for j, motor in enumerate(motors):
+            motor.setPosition(desired_command[j])
+
+        if desired_command[-1]:
+            set_gripper_normalized(1) # closed
+           
+        else:
+            set_gripper_normalized(0) # open
+
+
+
+        tt += 1
+finally:
+    if _HAS_CV2:
+        try:
+            cv2.destroyAllWindows()
+        except Exception:
+            pass
